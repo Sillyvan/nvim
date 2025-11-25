9e7555e2cc3c496d40a3f1b6195fad61004fac12
Silvan Gehrig <38138071+Sillyvan@users.noreply.github.com>
2025-11-25 16:22:06 +0100

    testing

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,11 +1,11 @@
-return {
-"vim-test/vim-test",
-  init = function()
-    vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<cr>")
-    vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<cr>")
-    vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<cr>")
-
-    vim.g['test#java#gradletest#options'] = '--info'
-    vim.g['test#runner_commands'] = { "RSpec", "MavenTest", "GradleTest" }
-  end,
-}
+-- return {
+-- 	"vim-test/vim-test",
+-- 	init = function()
+-- 		vim.keymap.set("n", "<leader>tn", "<cmd>TestNearest<cr>")
+-- 		vim.keymap.set("n", "<leader>tf", "<cmd>TestFile<cr>")
+-- 		vim.keymap.set("n", "<leader>tl", "<cmd>TestLast<cr>")
+--
+-- 		vim.g["test#java#gradletest#options"] = "--info"
+-- 		vim.g["test#runner_commands"] = { "RSpec", "MavenTest", "GradleTest" }
+-- 	end,
+-- }
