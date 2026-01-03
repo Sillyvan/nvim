adc0575f66995a0fa44f17481cad1ba309e5bd6e
Silvan Gehrig <38138071+Sillyvan@users.noreply.github.com>
2026-01-04 00:52:06 +0100

    some updates

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,35 +1,35 @@
 return {
-  'stevearc/conform.nvim',
-  config = function()
-    local conform = require 'conform'
+	"stevearc/conform.nvim",
+	config = function()
+		local conform = require("conform")
 
-    conform.setup {
-      format_on_save = {
-        timeout_ms = 2500,
-        lsp_fallback = true,
-      },
-      formatters_by_ft = {
-        javascript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-        javascriptreact = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-        typescript = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-        typescriptreact = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-        svelte = { 'prettierd', 'prettier', stop_after_first = true },
-        json = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-        jsonc = { 'biome', 'prettierd', 'prettier', stop_after_first = true },
-        yaml = { 'prettierd', 'prettier', stop_after_first = true },
-        html = { 'prettierd', 'prettier', stop_after_first = true },
-        css = { 'prettierd', 'prettier', stop_after_first = true },
-        scss = { 'prettierd', 'prettier', stop_after_first = true },
-        lua = { 'stylua' },
-        astro = { 'astro', 'prettierd', 'prettier', stop_after_first = true },
-      },
-    }
+		conform.setup({
+			format_on_save = {
+				timeout_ms = 2500,
+				lsp_fallback = true,
+			},
+			formatters_by_ft = {
+				javascript = { "biome", "prettierd", "prettier", stop_after_first = true },
+				javascriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
+				typescript = { "biome", "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
+				svelte = { "prettierd", "prettier", stop_after_first = true },
+				json = { "biome", "prettierd", "prettier", stop_after_first = true },
+				jsonc = { "biome", "prettierd", "prettier", stop_after_first = true },
+				yaml = { "prettierd", "prettier", stop_after_first = true },
+				html = { "prettierd", "prettier", stop_after_first = true },
+				css = { "prettierd", "prettier", stop_after_first = true },
+				scss = { "prettierd", "prettier", stop_after_first = true },
+				lua = { "stylua" },
+				astro = { "astro", "prettierd", "prettier", stop_after_first = true },
+			},
+		})
 
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      pattern = "*",
-      callback = function(args)
-        conform.format({ bufnr = args.buf })
-      end,
-    })
-  end,
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				conform.format({ bufnr = args.buf })
+			end,
+		})
+	end,
 }
--- lua/plugins/treesiter.lua
+++ lua/plugins/treesiter.lua
@@ -1,32 +1,32 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    local config = require 'nvim-treesitter.configs'
-    config.setup {
-      highlight = { enable = true },
-      indent = { enable = true },
-      auto_install = true,
-      ensure_installed = {
-        'javascript',
-        'jsdoc',
-        'bash',
-        'typescript',
-        'json',
-        'html',
-        'css',
-        'scss',
-        'svelte',
-        'lua',
-        'python',
-        'rust',
-        'go',
-        'yaml',
-        'toml',
-        'astro',
-        'markdown',
-      },
-    }
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({
+			highlight = { enable = true },
+			indent = { enable = true },
+			auto_install = true,
+			ensure_installed = {
+				"javascript",
+				"jsdoc",
+				"bash",
+				"typescript",
+				"json",
+				"html",
+				"css",
+				"scss",
+				"svelte",
+				"lua",
+				"python",
+				"rust",
+				"go",
+				"yaml",
+				"toml",
+				"astro",
+				"markdown",
+			},
+		})
+	end,
 }
